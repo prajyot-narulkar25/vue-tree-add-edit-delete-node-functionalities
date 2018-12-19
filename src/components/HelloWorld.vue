d467adaa3b6c35da02a75f65489f6d2cf27b36de
PRAJYOT-PC\PRAJYOT <prajyot@bittint.com>
2018-12-19 15:16:07 +0530

    tree functionalities

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,191 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="examples">
+        <div class="example">
+          <div class="example-tree">
+            <tree :data="data" :options="opts" :filter="filter" ref="tree">
+              <div slot-scope="{ node }" class="node-container">
+                <div class="node-text">{{ node.text }}</div>
+                <div class="node-controls">
+                  <a href="#" @mouseup.stop="editNode(node)">Edit&nbsp;</a>
+                  <a href="#" @mouseup.stop="removeNode(node)">Remove&nbsp;</a>
+                  <a href="#" @mouseup.stop="addChildNode(node)">Add child</a>
+                </div>
+              </div>
+            </tree>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+     data: () => ({
+
+          data: getData(),
+          filter: null,
+          opts: {
+          	minFetchDelay: 1000,
+            fetchData: (node) => {
+            	return Promise.resolve(data[node.id - 1])
+            },
+          }
+        }),
+
+        filters: {
+          time(time) {
+            return +time
+          }
+        },
+
+        methods: {
+          editNode(node, e) {
+            node.startEditing()
+          },
+
+          removeNode(node) {
+            if (confirm('Are you sure?')) {
+              node.remove()
+            }
+          },
+
+          addChildNode(node) {
+            if (node.enabled()) {
+            	node.append('New Node')
+            }
+          },
+
+          initEventViewer(event) {
+            const events = this.events
+
+            return function (node, newNode) {
+              let nodeText = '-'
+              let targetNode = newNode && newNode.text ? newNode : node
+
+              if (targetNode && targetNode.text) {
+                nodeText = targetNode.text
+              }
+
+              events.push(
+                Object.assign(
+                  {},
+                  event,
+                  { time: new Date, nodeText, id: key++ }
+                )
+              )
+
+              console.log(event, arguments)
+            }
+          }
+        }
+      }
+
+      function getData() {
+        return new Promise(resolve => {
+          setTimeout(_ => {
+            resolve([
+              {
+                "text": "Introduction",
+                "isBatch": true,
+                "id": 1
+              },
+              {
+                "text": "Part I: Fundamentals",
+                "isBatch": true,
+                "id": 2
+              },
+              {
+                "text": "Data Types and Variables",
+                "isBatch": true,
+                "id": 3
+              }
+            ])
+          }, 100)
+        })
+      }
+      var data = [
+	[
+    { "text": "Who Should Read This Book?" },
+    { "text": "How to Read This Book" },
+    { "text": "What’s in This Book?" },
+    { "text": "Have Fun!" }
+  ],
+  [
+    { "text": "Naming Variables" },
+    { "text": "Creating New Variables Using Math", "state": { "disabled": true } },
+    { "text": "Incrementing and Decrementing" },
+    { "text": "+= (plus-equals) and –= (minus-equals)" }
+  ],
+  [
+    { "text": "Interactive Programming" },
+    { "text": "Find the Buried Treasure!", "state": { "disabled": true, "expanded": true }, "children": [
+      { "text": "#1: A Snowman-Drawing Function" },
+      { "text": "#2: Drawing an Array of Points" },
+      { "text": "#3: Painting with Your Mouse" },
+      { "text": "#4: Drawing the Man in Hangman" }
+    ]},
+    { "text": "Object-Oriented Programming" }
+  ]
+
+
+]
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.demo-tree {
+  width: 50%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.filter-field {
+  display: block;
+  width: 100%;
+  padding: 3px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.events-table {
+  width: 100%;
+  margin-top: 15px;
 }
-a {
-  color: #42b983;
+
+.events-table-header {
+  padding: 8px;
+  line-height: 1.42857143;
+  vertical-align: top;
+  background-color: #fff;
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+
+}
+
+.events-table .event:nth-of-type(odd) {
+  background-color: #f9f9f9;
+}
+
+.events-table .event {
+  padding: 8px;
+  line-height: 1.42857143;
+  vertical-align: top;
+  border-top: 1px solid #ddd;
+  display: flex;
+}
+
+.events-table .event > span,
+.events-table-header > span {
+  flex-basis: 25%;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .8s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.node-controls a {
+  color: #ff14ce
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import LiquorTree from 'liquor-tree'
+Vue.use(LiquorTree)
 Vue.config.productionTip = false
 
 new Vue({
